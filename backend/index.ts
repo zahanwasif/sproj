75930c8d69c4195b9bc8dd37a4ec896878858fdc
Zahan Wasif <zahan.wasif@gmail.com>
2021-01-27 18:44:38 +0500

    added packages

---
+++ backend/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello world!');
+});
+
+app.listen(PORT, () => {
+	console.log(`Server has been started at http://localhost:${PORT}`);
+});
